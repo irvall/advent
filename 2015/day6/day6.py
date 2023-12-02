3509bf6bc026c0f4c569be40c336d96c9936f2bd
Johan Irvall <johanirvall@pm.me>
2023-12-02 09:31:18 +0100

    day2

--- 2015/day6/day6.py
+++ 2015/day6/day6.py
@@ -1,6 +1,6 @@
-import sys
+import sys, re
 
-use_test_data = False 
+use_test_data = False
 lines = [line.rstrip('\n') for line in open('mini.txt' if use_test_data else 'input.txt')]
 
 """ Puzzle input sneak peek:
@@ -10,7 +10,23 @@ turn off 341,304 t
 
 def part_one():
     ans = 0
-    ...
+    pattern = r'\d+'
+    lights = [[False for _ in range(1000)] for _ in range(1000)]
+    for line in lines:
+        x1, y1, x2, y2 = (int(_) for _ in re.findall(pattern, line))
+        turn_on = 'on' in line
+        while y1 <= y2:
+            while x1 <= x2:
+                if turn_on:
+                    lights[y1][x1] = True
+                else:
+                    toggle = 'toggle' in line
+                    lights[y1][x1] = not lights[y1][x1] if toggle else False
+                x1 += 1
+            y1 += 1
+    for row in lights:
+        for b in row:
+            ans += 1 if b else 0
     return ans
 
 def part_two():
@@ -19,7 +35,7 @@ def part_two():
     return ans
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     p1, p2 = part_one(), part_two()
     if p1: print('part one:', p1)
     if p2: print('part two:', p2)
---
+++ 2023/day2/day2.py
@@ -0,0 +1,55 @@
+import sys
+
+use_test_data = not False
+lines = [line.rstrip('\n') for line in open('mini.txt' if use_test_data else 'input.txt')]
+
+""" Puzzle input sneak peek:
+Game 1: 9 red, 5 blue, 6 green; 6 red, 13 blue; 2
+"""
+
+def part_one():
+    ans = 0
+    possible_games = []
+    for line in lines:
+        game, cubes = line.split(':')
+        game_id = int(game.split()[-1])
+        game_possible = True
+        for subset in cubes.split(';'):
+            tr, tg, tb = 12, 13, 14
+            for cube in subset.split(','):
+                count, color = cube.split()
+                count = int(count)
+                match color:
+                    case 'red': tr -= count
+                    case 'green': tg -= count
+                    case 'blue': tb -= count
+            if tr < 0 or tg < 0 or tb < 0:
+                game_possible = False
+                break
+        if game_possible:
+            possible_games.append(game_id)
+    return sum(possible_games)
+
+def part_two():
+    ans = 0
+    powers = []
+    for line in lines:
+        tr, tg, tb = 0, 0, 0
+        game, cubes = line.split(':')
+        for subset in cubes.split(';'):
+            for cube in subset.split(','):
+                count, color = cube.split()
+                count = int(count)
+                match color:
+                    case 'red': tr = max(tr, count)
+                    case 'green': tg = max(tg, count)
+                    case 'blue': tb = max(tb, count)
+        powers.append(tr*tg*tb)
+    return sum(powers)
+
+
+
+if __name__ == '__main__':
+    p1, p2 = part_one(), part_two()
+    if p1: print('part one:', p1)
+    if p2: print('part two:', p2)
