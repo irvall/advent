aedaa0bfd997a86e968e917668ccbd45627f7717
Johan Irvall <johanirvall@pm.me>
2023-12-03 13:18:29 +0100

    day3 update

--- 2023/day3/day3.py
+++ 2023/day3/day3.py
@@ -1,4 +1,4 @@
-import sys, re
+import sys, re, functools
 
 use_test_data = False
 lines = [line.rstrip('\n') for line in open('mini.txt' if use_test_data else 'input.txt')]
@@ -10,54 +10,45 @@ lines = [line.rstrip('\n') for line in open('mini.txt' if use_test_data else 'in
 def part_one():
     ans = 0
     text = ''.join(lines)
-    numbers = re.finditer(r'\d+', text)
     total_lines = len(lines)
     line_len = len(lines[0])
-    for number in numbers:
+    for number in re.finditer(r'\d+', text):
         num = int(number.group())
         start, end = number.span()
-        n = end-start
+        n = end-start + 1
         x, y = start % line_len, start // total_lines
         found = False
         for j in range(max(0, y-1), min(total_lines, y+2)):
-            for i in range(max(0, x-1), min(line_len, x+n+1)):
+            for i in range(max(0, x-1), min(line_len, x+n)):
                 ch = lines[j][i]
-                if not ch.isnumeric() and ch != '.':
-                    ans += num
-                    found = True
-                    break
-            if found: break
+                if ch.isnumeric() or ch == '.': continue
+                ans += num
+                found = True
+                break
+        if found: break
     return ans
 
 def part_two():
-    ans = 0
     text = ''.join(lines)
-    numbers = re.finditer(r'\d+', text)
     total_lines = len(lines)
     line_len = len(lines[0])
     gears = {}
-    for number in numbers:
+    for number in re.finditer(r'\d+', text):
         num = int(number.group())
         start, end = number.span()
-        n = end-start
+        n = end-start + 1
         x,y = start % line_len, start // total_lines
         found = False
         for j in range(max(0, y-1), min(total_lines, y+2)):
-            for i in range(max(0, x-1), min(line_len, x+n+1)):
-                ch = lines[j][i]
-                if ch == '*':
-                    if (j,i) in gears:
-                        gears[(j,i)].append(num)
-                    else:
-                        gears[(j,i)] = [num]
-                    found = True
-                    break
+            for i in range(max(0, x-1), min(line_len, x+n)):
+                if lines[j][i] != '*': continue
+                if (j,i) in gears: gears[(j,i)].append(num)
+                else: gears[(j,i)] = [num]
+                found = True
+                break
             if found: break
-    for k,v in gears.items():
-        if len(v) == 2:
-            a,b = v
-            ans += a*b
-    return ans
+    values = [a * b for values in gears.values() if len(values) == 2 for a, b in [values]]
+    return sum(values)
 
 if __name__ == '__main__':
     p1, p2 = part_one(), part_two()
