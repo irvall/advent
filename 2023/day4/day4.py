a4b0e6ae2eb36421f25bed34fe8f6f0c1558f909
Johan Irvall Rasmussen     Distribution og DevOps <jrjr@m89833.net.dr.dk>
2023-12-04 13:16:31 +0100

    day4 p2

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,6 +1,6 @@
-import sys
+from collections import defaultdict
 
-use_test_data = False 
+use_test_data = False
 lines = [line.rstrip('\n') for line in open('mini.txt' if use_test_data else 'input.txt')]
 
 """ Puzzle input sneak peek:
@@ -10,7 +10,7 @@ Card   1: 99 71 95 70 36 79 78 84 31 10 |  5 45 54
 def part_one():
     ans = 0
     for line in lines:
-        card, numbers = line.split(':')
+        _, numbers = line.split(':')
         scratch, winning = (set(_.split()) for _ in numbers.split('|'))
         hits = scratch.intersection(winning)
         ps = 1 if hits else 0
@@ -20,32 +20,19 @@ def part_one():
     return ans
 
 def part_two():
-    ans = 1
-    freq = {}
+    freq = defaultdict(int)
     for line in lines:
         card, numbers = line.split(':')
-        _, card_id = card.split()
-        card_id = int(card_id)
-        scratch, winning = (set(_.split()) for _ in numbers.split('|'))
-        hits = scratch.intersection(winning)
-        matches = len(hits)
-        times = 1 if card_id not in freq else freq[card_id]
-        while times > 0:
-            for i in range(card_id, card_id + matches + 1):
-                if i not in freq:
-                    freq[i] = 1
-                else:
-                    freq[i] += 1
-            times -= 1
-
-    print(freq)
+        card_id = int(card.split()[1])
+        scratch, ticket = (set(_.split()) for _ in numbers.split('|'))
+        copies = len(scratch & ticket)
+        freq[card_id] += 1
+        for i in range(card_id + 1, card_id + copies + 1):
+            freq[i] += freq[card_id]
     return sum(freq.values())
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     p1, p2 = part_one(), part_two()
     if p1: print('part one:', p1)
     if p2: print('part two:', p2)
-
-# too low
-# 14152603
