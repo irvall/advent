aa32e177bae7e5fce535fc6f1559b627617a8a42
Johan Irvall Rasmussen <jrjr@dr.dk>
2024-12-01 15:38:34 +0100

    update

--- 2024/day1/solution.py
+++ 2024/day1/solution.py
@@ -6,16 +6,16 @@ file_to_read = 'mini.txt' if use_test else 'input.txt'
 lines = [line.rstrip() for line in open(file_to_read, 'r').readlines()]
 
 ls, rs = [], []
-rfreq = defaultdict(int)
+freq = defaultdict(int)
 
 for line in lines:
-    lnum, rnum = [int(_) for _ in line.split()]
-    ls.append(lnum)
-    rs.append(rnum)
-    rfreq[rnum] += 1
+    left, right = (int(_) for _ in line.split())
+    ls.append(left)
+    rs.append(right)
+    freq[right] += 1
     
 dist = sum(abs(a-b) for a,b in zip(sorted(ls), sorted(rs)))
 print(dist)
 
-score = sum(l * rfreq[l] for l in ls)
+score = sum(l * freq[l] for l in ls)
 print(score)
