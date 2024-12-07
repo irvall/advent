034a803ca47a8ee6c5dd573503bb69e1c7b08e79
Johan Irvall Rasmussen <johanirvall@gmail.com>
2024-12-07 10:47:34 +0100

    Update solution.py

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -1,14 +1,14 @@
 import sys
 lines = [x.rstrip() for x in open(sys.argv[1], 'r').readlines()]
 
-def solve(numbers, current, target, i):
+def solve(numbers, current, target):
   if len(numbers) == 0:
     return current == target
   head, *tail = numbers
   concat = int(str(current) + str(head))
-  add = solve(tail, current+head, target, i+1)
-  mul = solve(tail, current*head, target, i+1)
-  con = solve(tail, concat, target, i+1)
+  add = solve(tail, current+head, target)
+  mul = solve(tail, current*head, target)
+  con = solve(tail, concat, target)
   return add or mul or con
 
 ans = 0
@@ -16,6 +16,6 @@ for line in lines:
   target, numbers = line.split(':')
   targetNumber = int(target)
   numbers = [int(_) for _ in numbers.split()]
-  if solve(numbers, 0, targetNumber, 0):
+  if solve(numbers, 0, targetNumber):
     ans += targetNumber
 print(ans)
